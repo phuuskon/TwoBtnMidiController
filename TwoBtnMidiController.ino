64f6217bcdfe4785538067845fbca0ee21e3b54e
Pekka Huuskonen <pekka.huuskonen@iki.fi>
2018-04-15 20:22:40 +0300

    added saving setting to EEPROM

--- TwoBtnMidiController.ino
+++ TwoBtnMidiController.ino
@@ -1,3 +1,4 @@
+
 /* 
  *  TwoBtnMidiController.ino
  *  Simple programmable two button midi controller
@@ -17,19 +18,23 @@
  *  
  *  TODO:
  *  - Setup for program change parameters in all modes
- *  - Saving and loading setup to/from EEPROM
  *  - Using 7 segment display for showing operation
  *  
+ *  DONE:
+ *  - Saving and loading setup to/from EEPROM
+ *    
  *  Current version uses following arduino libraries:
  *  OneButton (https://github.com/mathertel/OneButton)
  *  Fsm (https://github.com/jonblack/arduino-fsm)
  *  MIDI (https://github.com/FortySevenEffects/arduino_midi_library)
+ *  EEPROM (https://www.arduino.cc/en/Reference/EEPROM)
  */
-
+const char* VERSION = "0.1";
 
 #include <OneButton.h>
 #include <Fsm.h>
 #include <MIDI.h>
+#include <EEPROM.h>
 
 // state triggers
 #define TO_RUN 1
@@ -55,22 +60,22 @@ OneButton btn1(A1, true);
 OneButton btn2(A2, true);
 
 // midi setup variables
-int midiChannel;
+//int midiChannel;
 
 // mode 1 variables
-const int mode1_lowlimitPC = 0;
-const int mode1_highlimitPC = 16;
+//const int mode1_lowlimitPC = 0;
+//const int mode1_highlimitPC = 16;
 int mode1_currentPC;
 
 // mode 2 variables
-int mode2_btn1PC;
-int mode2_btn2PC;
+//int mode2_btn1PC;
+//int mode2_btn2PC;
 
 // mode 3 variables
-int mode3_btn1PC1;
-int mode3_btn1PC2;
-int mode3_btn2PC1;
-int mode3_btn2PC2;
+//int mode3_btn1PC1;
+//int mode3_btn1PC2;
+//int mode3_btn2PC1;
+//int mode3_btn2PC2;
 int mode3_current_btn1PC;
 int mode3_current_btn2PC;
 
@@ -88,32 +93,60 @@ State state_setup_midi(&on_setup_midi_enter, NULL, NULL);
 State state_setup_mode1(&on_setup_mode1_enter, NULL, NULL);
 State state_setup_mode2(&on_setup_mode2_enter, NULL, NULL);
 State state_setup_mode3(&on_setup_mode3_enter, NULL, NULL);
-State state_setup_exit(&on_setup_exit_enter, NULL, NULL);
+State state_setup_exit(&on_setup_exit_enter, NULL, &on_setup_exit_exit);
 Fsm main_fsm(&state_run);  
+
+// settings
+const int CONFIG_VERSION = 1; // ID of the settings block, change if struct is modified
+
+struct settingsStruct {
+  byte sversion;
+  byte midiChannel;
+  byte mode1_lowlimitPC, mode1_highlimitPC;
+  byte mode2_btn1PC, mode2_btn2PC;
+  byte mode3_btn1PC1, mode3_btn1PC2, mode3_btn2PC1, mode3_btn2PC2; 
+} _settings;
+
+const byte CONFIG_START_ADDRESS = 0;
 
 void setup() {
   // TODO: read setup from EEPROM
   
   Serial.begin(9600);
 
+  // load settings
+  readSettings();
+
+  // DEBUG: print settings to serial
+  Serial.println("setting version: "+String(_settings.sversion));
+  Serial.println("midiChannel: "+String(_settings.midiChannel));
+  Serial.println("mode1_lowlimitPC: "+String(_settings.mode1_lowlimitPC));
+  Serial.println("mode1_highlimitPC: "+String(_settings.mode1_highlimitPC));
+  Serial.println("mode2_btn1PC: "+String(_settings.mode2_btn1PC));
+  Serial.println("mode2_btn2PC: "+String(_settings.mode2_btn2PC));
+  Serial.println("mode3_btn1PC1: "+String(_settings.mode3_btn1PC1));
+  Serial.println("mode3_btn1PC2: "+String(_settings.mode3_btn1PC2));
+  Serial.println("mode3_btn2PC1: "+String(_settings.mode3_btn2PC1));
+  Serial.println("mode3_btn2PC2: "+String(_settings.mode3_btn2PC2));
+  
   // init midi channel
-  midiChannel = 1;
+  //midiChannel = 1;
   //MIDI.begin(midiChannel);
 
   // init mode 1
-  mode1_currentPC = mode1_lowlimitPC;
+  mode1_currentPC = _settings.mode1_lowlimitPC;
 
   // init mode 2
-  mode2_btn1PC = 17;
-  mode2_btn2PC = 18;
+  //mode2_btn1PC = 17;
+  //mode2_btn2PC = 18;
 
   // init mode 3
-  mode3_btn1PC1 = 20;
-  mode3_btn1PC2 = 21;
-  mode3_btn2PC1 = 22;
-  mode3_btn2PC2 = 23;
-  mode3_current_btn1PC = mode3_btn1PC1;
-  mode3_current_btn2PC = mode3_btn2PC1;
+  //mode3_btn1PC1 = 20;
+  //mode3_btn1PC2 = 21;
+  //mode3_btn2PC1 = 22;
+  //mode3_btn2PC2 = 23;
+  mode3_current_btn1PC = _settings.mode3_btn1PC1;
+  mode3_current_btn2PC = _settings.mode3_btn2PC1;
 
   // main_fsm transitions
   main_fsm.add_transition(&state_run, &state_setup_mode, TO_SETUP_MODE, NULL);
@@ -215,6 +248,12 @@ void on_setup_exit_enter()
   btn2.attachLongPressStop(longPressToRun);
 }
 
+void on_setup_exit_exit()
+{
+  Serial.println("on_setup_exit_exit");
+  
+}
+
 void longPressStart1()
 {
   // set btn2 state to pressed
@@ -311,6 +350,13 @@ void longPressToRun()
 {
   if(btn1_state == 1 && btn2_state == 1)
   {
+    // save setting
+    saveSettings();
+    main_fsm.trigger(TO_RUN);  
+  }
+  else if(btn1_state == 1 && btn2_state == 0)
+  {
+    // only button one longpressed, goto run without saving
     main_fsm.trigger(TO_RUN);  
   }
   btn2_state = 0;
@@ -319,69 +365,69 @@ void longPressToRun()
 
 void cyclePcDown()
 {
-  if(mode1_currentPC == mode1_lowlimitPC)
+  if(mode1_currentPC == _settings.mode1_lowlimitPC)
   {
-    mode1_currentPC = mode1_highlimitPC;
+    mode1_currentPC = _settings.mode1_highlimitPC;
   }
   else 
   {
     mode1_currentPC--;
   }
-  Serial.println("send midi pc: "+String(mode1_currentPC)+" ch:"+String(midiChannel));
+  Serial.println("send midi pc: "+String(mode1_currentPC)+" ch:"+String(_settings.midiChannel));
   //MIDI.sendProgramChange(mode1_currentPC, midiChannel);
 }
 
 void cyclePcUp()
 {
-  if(mode1_currentPC == mode1_highlimitPC)
+  if(mode1_currentPC == _settings.mode1_highlimitPC)
   {
-    mode1_currentPC = mode1_lowlimitPC;
+    mode1_currentPC = _settings.mode1_lowlimitPC;
   }
   else 
   {
     mode1_currentPC++;
   }
-  Serial.println("send midi pc: "+String(mode1_currentPC)+" ch:"+String(midiChannel));
+  Serial.println("send midi pc: "+String(mode1_currentPC)+" ch:"+String(_settings.midiChannel));
   //MIDI.sendProgramChange(mode1_currentPC, midiChannel);
 }
 
 void sendBtn1PC()
 {
-  Serial.println("send midi pc: "+String(mode2_btn1PC)+" ch:"+String(midiChannel));
-  //MIDI.sendProgramChange(mode2_btn1PC, midiChannel);
+  Serial.println("send midi pc: "+String(_settings.mode2_btn1PC)+" ch:"+String(_settings.midiChannel));
+  //MIDI.sendProgramChange(_settings.mode2_btn1PC, midiChannel);
 }
 
 void sendBtn2PC()
 {
-  Serial.println("send midi pc: "+String(mode2_btn2PC)+" ch:"+String(midiChannel));
-  //MIDI.sendProgramChange(mode2_btn2PC, midiChannel);
+  Serial.println("send midi pc: "+String(_settings.mode2_btn2PC)+" ch:"+String(_settings.midiChannel));
+  //MIDI.sendProgramChange(_settings.mode2_btn2PC, midiChannel);
 }
 
 void sendBtn1TogglePC()
 {
-  if(mode3_current_btn1PC == mode3_btn1PC1)
+  if(mode3_current_btn1PC == _settings.mode3_btn1PC1)
   {
-    mode3_current_btn1PC = mode3_btn1PC2;
+    mode3_current_btn1PC = _settings.mode3_btn1PC2;
   }
   else
   {
-    mode3_current_btn1PC = mode3_btn1PC1;
+    mode3_current_btn1PC = _settings.mode3_btn1PC1;
   }
-  Serial.println("send midi pc: "+String(mode3_current_btn1PC)+" ch:"+String(midiChannel));
+  Serial.println("send midi pc: "+String(mode3_current_btn1PC)+" ch:"+String(_settings.midiChannel));
   //MIDI.sendProgramChange(mode3_current_btn1PC, midiChannel);
 }
 
 void sendBtn2TogglePC()
 {
-  if(mode3_current_btn2PC == mode3_btn2PC1)
+  if(mode3_current_btn2PC == _settings.mode3_btn2PC1)
   {
-    mode3_current_btn2PC = mode3_btn2PC2;
+    mode3_current_btn2PC = _settings.mode3_btn2PC2;
   }
   else
   {
-    mode3_current_btn2PC = mode3_btn2PC1;
+    mode3_current_btn2PC = _settings.mode3_btn2PC1;
   }
-  Serial.println("send midi pc: "+String(mode3_current_btn2PC)+" ch:"+String(midiChannel));
+  Serial.println("send midi pc: "+String(mode3_current_btn2PC)+" ch:"+String(_settings.midiChannel));
   //MIDI.sendProgramChange(mode3_current_btn2PC, midiChannel);
 }
 
@@ -413,27 +459,67 @@ void modeUp()
 
 void midiChnDown()
 {
-  if(midiChannel == 1)
+  if(_settings.midiChannel == 1)
   {
-    midiChannel = 16;
+    _settings.midiChannel = 16;
   }
   else
   {
-    midiChannel--;
+    _settings.midiChannel--;
   }
-  Serial.println("midi chn set to "+String(midiChannel));
+  Serial.println("midi chn set to "+String(_settings.midiChannel));
 }
 
 void midiChnUp()
 {
-  if(midiChannel == 16)
+  if(_settings.midiChannel == 16)
   {
-    midiChannel = 1;
+    _settings.midiChannel = 1;
   }
   else
   {
-    midiChannel++;
+    _settings.midiChannel++;
   }
-  Serial.println("midi chn set to "+String(midiChannel));
+  Serial.println("midi chn set to "+String(_settings.midiChannel));
+}
+
+void loadDefaultSettings(){
+  Serial.println("loading default settings");
+  _settings.sversion = CONFIG_VERSION;
+  _settings.midiChannel = 1;
+  _settings.mode1_lowlimitPC = 0;
+  _settings.mode1_highlimitPC = 16; 
+  _settings.mode2_btn1PC = 17; 
+  _settings.mode2_btn2PC = 18;
+  _settings.mode3_btn1PC1 = 19;
+  _settings.mode3_btn1PC2 = 20;
+  _settings.mode3_btn2PC1 = 21;
+  _settings.mode3_btn2PC2 = 22;
+}
+
+void saveSettings() 
+{
+   EEPROM.put(CONFIG_START_ADDRESS, _settings);
+   Serial.println("settings saved!");
+}
+
+void readSettings()
+{
+  EEPROM.get(CONFIG_START_ADDRESS, _settings);
+  // DEBUG: print settings to serial
+  Serial.println("loaded settings:");
+  Serial.println("version: "+String(_settings.sversion));
+  Serial.println("midiChannel: "+String(_settings.midiChannel));
+  Serial.println("mode1_lowlimitPC: "+String(_settings.mode1_lowlimitPC));
+  Serial.println("mode1_highlimitPC: "+String(_settings.mode1_highlimitPC));
+  Serial.println("mode2_btn1PC: "+String(_settings.mode2_btn1PC));
+  Serial.println("mode2_btn2PC: "+String(_settings.mode2_btn2PC));
+  Serial.println("mode3_btn1PC1: "+String(_settings.mode3_btn1PC1));
+  Serial.println("mode3_btn1PC2: "+String(_settings.mode3_btn1PC2));
+  Serial.println("mode3_btn2PC1: "+String(_settings.mode3_btn2PC1));
+  Serial.println("mode3_btn2PC2: "+String(_settings.mode3_btn2PC2));
+  if (_settings.sversion != CONFIG_VERSION) {
+    loadDefaultSettings();
+  }
 }
 
